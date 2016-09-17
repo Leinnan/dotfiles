8daeac727978c4ddbdeaec191df15b3666626a60
Leinnan <rybopiotr@gmail.com>
2016-09-17 23:46:09 +0200

    Little update

--- .scripts/not_to_dark.sh
+++ .scripts/not_to_dark.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-float_scale=2
-function float_cond()
-{
-    local cond=0
-    if [[ $# -gt 0 ]]; then
-        cond=$(echo "$*" | bc -q 2>/dev/null)
-        if [[ -z "$cond" ]]; then cond=0; fi
-        if [[ "$cond" != 0  &&  "$cond" != 1 ]]; then cond=0; fi
-    fi
-    local stat=$((cond == 0))
-    return $stat
-}
 
 while [ 1 ]
 do
-    jasnosc=$(xbacklight)
-    ##if [ $jasnosc -lt 10.0 ]
-    if float_cond '10.5 > $jasnosc'
+    luminosity=$(xbacklight)
+    minimal_lumonisity=1.5
+    if (( $(echo "$minimal_lumonisity > $luminosity" | bc -l) ))
     then
-        xbacklight -set 10.5
+        xbacklight -set $minimal_lumonisity
+        xbacklight +2
         echo "TOO DARK"
+    else
+        echo "OKAY!"
     fi
     sleep 0.5
 done
